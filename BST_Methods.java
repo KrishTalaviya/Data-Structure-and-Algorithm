1e1dcd42730f5fb7f7d296b5ec9e4ca2f89b8580
KrishTalaviya <93860565+KrishTalaviya@users.noreply.github.com>
2022-09-07 11:23:33 +0530

    Update BST_Methods.java

--- BST_Methods.java
+++ BST_Methods.java
@@ -72,17 +72,78 @@ class Bst{
    		}
    	}
    }
+   void delete_leafNode(int a){
+   	Node parent=null;
+   	Node temp=root;
+   	Node t;
+   	while(temp!=null && temp.data!=a){
+   		parent=temp;
+   		if(a>temp.data){
+   			temp=temp.right;
+   		}
+   		else{
+   		  temp=temp.left;
+   		}
+   		
+   	}
+   	if(temp.left==null && temp.right==null){
+   		if(temp.data>parent.data){
+           parent.right=null;
+   		}
+   		else{
+   			parent.left=null;
+   		}
+   	}
+   	if(temp.left!=null && temp.right==null){
+          parent.left=temp.left;
+   	}
+   	if(temp.left==null && temp.right!=null){
+         parent.right=temp.right;
+   	}
+   	if(temp.left!=null && temp.right!=null){
+
+         t=inOrderSuccesor(temp);
+         temp.data=t.data;
+     }
+  }
+   Node inOrderSuccesor(Node temp){
+      boolean rightchild=true;
+      Node parent=temp;
+      temp=temp.right;
+      if(temp.left!=null){
+      	rightchild=false;
+      }
+      while(temp.left!=null){
+       parent=temp;
+       temp=temp.left;
+
+      }
+      if(rightchild){
+      	parent.right=temp.right;
+      }
+      else{
+      	parent.left=temp.left;
+      }
+      return temp;
+   }
+
 }
 class BST_Methods{
 	public static void main(String[] args) {
 		Bst bstree=new Bst();
+		bstree.insert_bst(5);
+		bstree.insert_bst(3);
 		bstree.insert_bst(8);
+		bstree.insert_bst(1);
 		bstree.insert_bst(10);
-		bstree.insert_bst(7);
 		bstree.insert_bst(9);
-		//bstree.preOrderDisplay();
-		bstree.inOrderTraversal();
+		bstree.insert_bst(100);
 		bstree.preOrderDisplay();
+		
+		bstree.delete_leafNode(10);
+		System.out.println("----");
+		bstree.preOrderDisplay();
+		
 		
 		
 		
